f8ed3fe6837f61561c804958cf94b5fc0c70745f
ryangholland <rghcode@gmail.com>
2024-08-03 11:39:11 -0400

    Style header, sidebar, footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,31 @@
-import '../css/style.css'
-import Logo from "../assets/format-list-checks.svg"
+import "../css/style.css";
+import logo from "../assets/format-list-checks.svg";
+import inbox from "../assets/inbox.svg";
+import today from "../assets/calendar-today.svg";
+import week from "../assets/calendar-week.svg";
+import plus from "../assets/plus.svg"
 
 import Todo from "./todo";
 import Project from "./project";
 import List from "./list";
 
 const logoImg = document.getElementById("logoImg");
-logoImg.src = Logo;
+logoImg.src = logo;
+const inboxImg = document.getElementById("inboxImg");
+inboxImg.src = inbox;
+const todayImg = document.getElementById("todayImg");
+todayImg.src = today;
+const weekImg = document.getElementById("weekImg");
+weekImg.src = week;
+const plusImgs = document.querySelectorAll(".plusImg");
+plusImgs.forEach(plusImg => {
+  plusImg.src = plus;
+})
 
 const myList = new List();
 
-const testProject = new Project("Chores")
-myList.addProject(testProject)
+const testProject = new Project("Chores");
+myList.addProject(testProject);
 
 const testBtn = document.getElementById("test-task");
 testBtn.addEventListener("click", () => {
@@ -24,6 +38,6 @@ testBtn.addEventListener("click", () => {
   );
   myList.addTodo(testTodo);
   myList.renderTodos();
-})
+});
 
 myList.renderTodos();
