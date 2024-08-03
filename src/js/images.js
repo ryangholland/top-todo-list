ad40464426d8464ce69cba1981c3c7bce8358b92
ryangholland <rghcode@gmail.com>
2024-08-03 11:42:38 -0400

    Move image code from index to image module

---
+++ src/js/images.js
@@ -0,0 +1,22 @@
+import logo from "../assets/format-list-checks.svg";
+import inbox from "../assets/inbox.svg";
+import today from "../assets/calendar-today.svg";
+import week from "../assets/calendar-week.svg";
+import plus from "../assets/plus.svg";
+
+const loadImages = () => {
+  const logoImg = document.getElementById("logoImg");
+  logoImg.src = logo;
+  const inboxImg = document.getElementById("inboxImg");
+  inboxImg.src = inbox;
+  const todayImg = document.getElementById("todayImg");
+  todayImg.src = today;
+  const weekImg = document.getElementById("weekImg");
+  weekImg.src = week;
+  const plusImgs = document.querySelectorAll(".plusImg");
+  plusImgs.forEach((plusImg) => {
+    plusImg.src = plus;
+  });
+};
+
+export default loadImages;
--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,11 @@
 import "../css/style.css";
-import logo from "../assets/format-list-checks.svg";
-import inbox from "../assets/inbox.svg";
-import today from "../assets/calendar-today.svg";
-import week from "../assets/calendar-week.svg";
-import plus from "../assets/plus.svg"
+import loadImages from "./images";
 
 import Todo from "./todo";
 import Project from "./project";
 import List from "./list";
 
-const logoImg = document.getElementById("logoImg");
-logoImg.src = logo;
-const inboxImg = document.getElementById("inboxImg");
-inboxImg.src = inbox;
-const todayImg = document.getElementById("todayImg");
-todayImg.src = today;
-const weekImg = document.getElementById("weekImg");
-weekImg.src = week;
-const plusImgs = document.querySelectorAll(".plusImg");
-plusImgs.forEach(plusImg => {
-  plusImg.src = plus;
-})
+loadImages();
 
 const myList = new List();
 
